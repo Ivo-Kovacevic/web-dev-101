02782590ecd70b15c61d27f0ae4a085b59d63323
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-11-21 21:04:30 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <>
+      <nav className="text-4xl text-center border-b-2 border-b-gray-700 p-4">
+        Kako započeti s web developmentom
+      </nav>
+      <main className="flex flex-col items-center">
+        <section className="p-4 max-w-[800px] w-full">
+          <h1 className="text-2xl">Uvod</h1>
+          <p>Ovo je uvod</p>
+        </section>
+      </main>
+    </>
+  );
+}
